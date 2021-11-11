c9f769ea58a62e6b8b2c4401251b71e0209f4a1c
Jeffrey Wu <xr-hui@qq.com>
2021-11-11 21:45:17 +0800

    feat: stop blur on scroll if selecting text

--- example/lib/full_page_editor.dart
+++ example/lib/full_page_editor.dart
@@ -377,9 +377,9 @@ class FullPageEditorState extends State<FullPageEditor> {
   }
 
   Future<void> _loadDocument() async {
-    // final result = await rootBundle.loadString('assets/sample_data.json');
-    // final doc = Document.fromJson(jsonDecode(result));
-    final doc = Document();
+    final result = await rootBundle.loadString('assets/sample_data.json');
+    final doc = Document.fromJson(jsonDecode(result));
+    // final doc = Document();
 
     final list = doc.toDelta().toJson();
     for (final i in list) {
