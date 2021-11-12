5c3ce479d2130ecf11f565e058b0fe4b9f7865ec
Jeffrey Wu <xr-hui@qq.com>
2021-11-12 20:02:19 +0800

    feat: make image and video inline

--- example/lib/full_page_editor.dart
+++ example/lib/full_page_editor.dart
@@ -435,83 +435,83 @@ class FullPageEditorState extends State<FullPageEditor> {
   }
 
   Future<void> _pickImage() async {
-    _controller.batchInsertEmbed(
-      appendNewLineAfterImage: true,
-      appendNewLineAfterVideo: false,
-      appendNewLine: false,
-      embeds: [
-        ImageEmbed(
-          name: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
-          source: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
-          checkPath: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
-          width: 230,
-          height: 230,
-        ),
-        VideoEmbed(
-          source: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
-          duration: 100,
-          thumbUrl: 'https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png',
-          thumbName: 'https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png',
-          fileType: 'mp4',
-          width: 100,
-          height: 200,
-        ),
-        // ImageEmbed(
-        //   name: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
-        //   source: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
-        //   checkPath: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
-        //   width: 230,
-        //   height: 230,
-        // ),
-        // VideoEmbed(
-        //   source: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
-        //   duration: 100,
-        //   thumbUrl: 'https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png',
-        //   thumbName: 'https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png',
-        //   fileType: 'mp4',
-        //   width: 100,
-        //   height: 200,
-        // ),
-        // ImageEmbed(
-        //   name: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
-        //   source: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
-        //   checkPath: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
-        //   width: 230,
-        //   height: 230,
-        // ),
-        // MentionEmbed(
-        //   denotationChar: '',
-        //   id: '1',
-        //   value: '#test',
-        //   prefixChar: '#',
-        // ),
-      ],
-    );
-    // final picker = ImagePicker();
-    // final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-    // if (image != null) {
-    //   _controller.insertImage(
-    //     name: image.name,
-    //     // source: 'file://${image.name}',
-    //     source: image.name,
-    //     checkPath: image.name,
-    //     width: 230,
-    //     height: 230,
-    //     ignoreFocus: true,
-    //     appendNewLine: true,
-    //   );
-    //   // _controller.insertVideo(
-    //   //   source:
-    //   //       'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
-    //   //   duration: 100,
-    //   //   thumbUrl: 'file://${image.name}',
-    //   //   thumbName: image.name,
-    //   //   fileType: 'mp4',
-    //   //   width: 100,
-    //   //   height: 200,
-    //   //   ignoreFocus: true,
-    //   // );
-    // }
+    // _controller.batchInsertEmbed(
+    //   appendNewLineAfterImage: true,
+    //   appendNewLineAfterVideo: false,
+    //   appendNewLine: false,
+    //   embeds: [
+    //     ImageEmbed(
+    //       name: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
+    //       source: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
+    //       checkPath: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
+    //       width: 230,
+    //       height: 230,
+    //     ),
+    //     VideoEmbed(
+    //       source: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
+    //       duration: 100,
+    //       thumbUrl: 'https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png',
+    //       thumbName: 'https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png',
+    //       fileType: 'mp4',
+    //       width: 100,
+    //       height: 200,
+    //     ),
+    //     // ImageEmbed(
+    //     //   name: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
+    //     //   source: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
+    //     //   checkPath: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
+    //     //   width: 230,
+    //     //   height: 230,
+    //     // ),
+    //     // VideoEmbed(
+    //     //   source: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
+    //     //   duration: 100,
+    //     //   thumbUrl: 'https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png',
+    //     //   thumbName: 'https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png',
+    //     //   fileType: 'mp4',
+    //     //   width: 100,
+    //     //   height: 200,
+    //     // ),
+    //     // ImageEmbed(
+    //     //   name: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
+    //     //   source: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
+    //     //   checkPath: "https://user-images.githubusercontent.com/122956/72955931-ccc07900-3d52-11ea-89b1-d468a6e2aa2b.png",
+    //     //   width: 230,
+    //     //   height: 230,
+    //     // ),
+    //     // MentionEmbed(
+    //     //   denotationChar: '',
+    //     //   id: '1',
+    //     //   value: '#test',
+    //     //   prefixChar: '#',
+    //     // ),
+    //   ],
+    // );
+    final picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      // _controller.insertImage(
+      //   name: image.name,
+      //   // source: 'file://${image.name}',
+      //   source: image.name,
+      //   checkPath: image.name,
+      //   width: 230,
+      //   height: 230,
+      //   ignoreFocus: true,
+      //   appendNewLine: true,
+      // );
+      _controller.insertVideo(
+        source:
+            'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
+        duration: 100,
+        thumbUrl: 'file://${image.name}',
+        thumbName: image.name,
+        fileType: 'mp4',
+        width: 100,
+        height: 200,
+        ignoreFocus: true,
+      );
+    }
   }
 
   Future<void> _showEditorDialog() async {
